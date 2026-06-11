09e40a24d0c099db5e74592253e32ded7caf1892
Diego Alderete <141648675+killerchipy723@users.noreply.github.com>
2026-06-11 11:12:34 -0300

    midificaciones okis

--- db.py
+++ db.py
@@ -3,16 +3,16 @@ from pymysql.cursors import DictCursor
 
 def get_connection():
     return pymysql.connect(
-        #host="200.58.106.156",
+      host="200.58.106.156",
+      port=3306,
+      user="c2710325_killer",
+      password="SistemaIES6021",
+      database="c2710325_muni",
+       # host="localhost",
         #port=3306,
-       # user="c2710325_killer",
-        #password="SistemaIES6021",
-        #database="c2710325_muni",
-        host="localhost",
-        port=3306,
-        user="root",
-        password="admin123",
-        database="muni",
+        #user="root",
+       # password="admin123",
+       # database="muni",
         charset="utf8mb4",
         cursorclass=DictCursor,
         autocommit=False
